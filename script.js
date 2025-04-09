8dd9ad23e387b84cbd90bb212c09e333b5f3a7c6
Christopher Hipper <hipper.christopher@gmail.com>
2025-04-09 21:12:49 +0200

    change fetch

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
+let count = 10;
 let pokemons = [];
-const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0"
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
 
-async function getPokemons() {
-    let response = await fetch(BASE_URL);
-    return responseToJson = await response.json()
-    
+async function getPokemons(path) {
+    let response = await fetch(BASE_URL + path);
+    let responseToJson = await response.json()
+    pokemons.push(responseToJson)
 }
 
  async function onloadFunc() {
-    let pokemonResponse = await getPokemons()
-    let pokemonArray = pokemonResponse.results
-    renderPokemons(pokemonArray)
-
+    for (let i = 1; i < count; i++) {
+        await getPokemons(i);
+    }
+    renderPokemons();
+    console.log(pokemons);
     
 } 
 
-function renderPokemons(pokemonArray) {
-    let contentRef = document.getElementById("content")
-    for (let index = 0; index < pokemonArray.length; index++) {
-        pokemons.push(
-            {
-                id : pokemonArray[index].name,                          
-                infos : pokemonArray[index].url
-            }
-        )
+function renderPokemons() {
+    let contentRef = document.getElementById('content')
+    contentRef.innerHTML = "";
+    for (let index = 0; index < pokemons.length; index++) {
         contentRef.innerHTML += getPokemonsTemplate(index)
     }
-    console.log(pokemons);
-    
-    console.log(pokemons[1].infos);
+}
+
+function getMorePokemons() {
+    pokemons.length = 0;
+    count = count + 5;
+    onloadFunc();
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,10 +1,10 @@
 function getPokemonsTemplate(i) {
     return `<div class="card">
                 <div class="card_header">
-                    <h3>#${i+1}</h3>
-                    <h3>${pokemons[i].id.charAt(0).toUpperCase() + pokemons[i].id.slice(1)}</h3>
+                    <h3>#${pokemons[i].id}</h3>
+                    <h3>${pokemons[i].name.charAt(0).toUpperCase() + pokemons[i].name.slice(1)}</h3>
                 </div>
-                <img src="" alt="großes bild">
+                <img src="${pokemons[i].sprites.other.home.front_default}" alt="großes bild">
                 <div>
                     <img src="" alt="+">
                     <img src="" alt="+">
