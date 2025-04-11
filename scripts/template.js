5a8f6bf0c737d9a18ebae1f957c7cf305dea94a9
ChristopherHipper <hipper.christopher@gmail.com>
2025-04-11 09:53:43 +0200

    Update template.js

--- scripts/template.js
+++ scripts/template.js
@@ -4,9 +4,10 @@ function getPokemonsTemplate(i) {
                     <h3>#${pokemons[i].id}</h3>
                     <h3>${pokemons[i].name.charAt(0).toUpperCase() + pokemons[i].name.slice(1)}</h3>
                 </div>
-                <img class="pokemon_img ${pokemons[i].types[0].type.name}" src="${pokemons[i].sprites.other.home.front_default}" alt="großes bild">
+                <img class="pokemon_img ${pokemons[i].type1}" src="${pokemons[i].img}" alt="großes bild">
                 <div>
-                    <img class="types_img" src="./assets/icon/${pokemons[i].types[0].type.name}.png" alt="+">
+                    <img class="types_img" src="./assets/icon/${pokemons[i].type1}.png" alt="+">
+                    <img class="types_img" src="./assets/icon/${pokemons[i].type2}.png" alt="+">
                 </div>
             </div>`
 }
